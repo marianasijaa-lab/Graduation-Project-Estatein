69ed78172ee729fde866e0028cb9e6a8ff66042d
Hasan <https://github.com/hasanebrahim2002/task-1-adv.git>
2026-08-25 03:49:34 +0300

    contact page

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -1,6 +1,8 @@
 import { SectionHeader } from "../components/common/SectionHeader";
+import OfficeLocations from "../components/OfficeLocations/OfficeLocations";
 import PageHero from "../components/sections/hero/PageHero";
 import ServiceCard from "../components/sections/services/ServiceCard";
+import TeamCard from "../components/TeamCard/TeamCard";
 import { services } from "../data/contactData";
 const Contact = () => {
   return (
@@ -11,7 +13,7 @@ const Contact = () => {
       />
       <section
         className="w-full bg-bg-dark-1 border border-bg-gray-1 py-1 sm:py-2"
-        style={{ boxShadow: '0px 0px 0px 6px #191919' }}
+        style={{ boxShadow: "0px 0px 0px 6px #191919" }}
       >
         <div className="w-full max-w-[1590px] mx-auto px-2 sm:px-1 lg:px-2">
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-2 sm:gap-3">
@@ -34,6 +36,17 @@ const Contact = () => {
           fullWidth
         />
       </div>
+
+      <div className="w-full max-w-[1590px] mx-auto px-6 sm:px-10 lg:px-16 pt-10 sm:pt-14">
+        <SectionHeader
+          title="Explore Estatein's World"
+          subtitle="Step inside the world of Estatein, where professionalism meets warmth, and expertise meets passion. Our gallery offers a glimpse into our team and workspaces, inviting you to get to know us better."
+          className="mb-10"
+          fullWidth
+        />
+      </div>
+      <OfficeLocations />
+      <TeamCard />
     </div>
   );
 };
