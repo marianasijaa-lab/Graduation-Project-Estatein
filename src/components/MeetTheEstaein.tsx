e803b4f5ae099ebde03012d7240fd2751837083b
mariana <sijaamimo@gmail.com>
2026-09-03 20:40:20 +0300

    added 1-4

--- src/components/MeetTheEstaein.tsx
+++ src/components/MeetTheEstaein.tsx
@@ -43,6 +43,7 @@ const MeetTheEstaein = () => {
         <SectionHeader
           title="Meet the Estatein Team"
           subtitle="At Estatein, our success is driven by the dedication and expertise of our team. Get to know the people behind our mission to make your real estate dreams a reality."
+          className="mb-8 sm:mb-14"
         />
         <TeamCardsList members={teamData} contactLabel="Say Hello 👋" />
       </div>
--- src/components/NavigatingTheEstatein.tsx
+++ src/components/NavigatingTheEstatein.tsx
@@ -42,6 +42,7 @@ export const NavigatingTheEstatein = () => {
         <SectionHeader
           title="Navigating the Estatein Experience"
           subtitle="At Estatein, we've designed a straightforward process to help you find and purchase your dream property with ease. Here's a step-by-step guide to how it all works."
+          className="mb-8 sm:mb-14"
         />
         <StepsCardsList steps={stepsData} />
       </div>
